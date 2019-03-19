7c7a4fb24c872566ec4106d09ac35f57700f68e6
Henrik <henrik@gyllensvard.com>
2019-03-19 16:06:20 +1100

    Initial commit showing the crash in action

---
+++ app/src/main/java/com/hgyllensvard/navLibCrash/AFragment.kt
@@ -0,0 +1,5 @@
+package com.hgyllensvard.navLibCrash
+
+import androidx.fragment.app.Fragment
+
+class AFragment : Fragment()
---
+++ app/src/main/java/com/hgyllensvard/navLibCrash/StartFragment.kt
@@ -0,0 +1,5 @@
+package com.hgyllensvard.navLibCrash
+
+import androidx.fragment.app.Fragment
+
+class StartFragment : Fragment()
